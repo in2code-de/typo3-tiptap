f594d088906e16feaf41bf2bef5fa9a34ef4ecb8
Felix Ranesberger <felix.ranesberger@in2code.de>
2025-07-04 14:57:46 +0200

    [TASK] Work on plugin system

--- build/frontend/src/index.ts
+++ build/frontend/src/index.ts
@@ -1,54 +1,7 @@
-import { css, html, LitElement } from 'lit'
-import { defineTipTapPlugin, getConfiguration } from './configuration.ts'
+import { defineTipTapPlugin } from './configuration.ts'
+import './tiptap/index.ts'
 import './styles.css'
-
-class EditorTipTap extends LitElement {
-  static styles = css`p { color: blue }`
-
-  render() {
-    return html`<p>Hello, I'm TipTap!</p>`
-  }
-}
-
-async function init(data: {
-  plugins: {
-    path: string
-    exports?: string[]
-  }[]
-}) {
-  await Promise.all(
-    data.plugins.map(async (plugin) => {
-      const module = await import(plugin.path)
-
-      if (plugin.exports) {
-        plugin.exports.forEach((exportName) => {
-          const exportData = module[exportName]
-
-          if (exportData && exportData === 'function') {
-            module[exportName]()
-          }
-          else {
-            console.warn(`in2tiptap: Export ${exportName} not found in ${plugin.path}`)
-          }
-        })
-      }
-      else {
-        if (module.default && typeof module.default === 'function') {
-          module.default()
-        }
-        else {
-          console.warn(`in2tiptap: No default export found in ${plugin.path} and no exports specified`)
-        }
-      }
-    }),
-  )
-
-  console.log(getConfiguration())
-
-  window.customElements.define('editor-tiptap', EditorTipTap)
-}
 
 export {
   defineTipTapPlugin,
-  init,
 }
