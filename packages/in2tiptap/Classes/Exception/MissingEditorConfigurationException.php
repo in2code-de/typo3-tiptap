93b1064a3aab4386c368ab1ae770164e9bfde7d9
Bastien Lutz <bastien.lutz@in2code.de>
2025-08-14 10:34:55 +0200

    [FEATURE] Prepare resolving of editor specific configurations

---
+++ packages/in2tiptap/Classes/Exception/MissingEditorConfigurationException.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace In2code\In2TipTap\Exception;
+
+class MissingEditorConfigurationException extends \Exception
+{
+}
--- packages/in2tiptap/Classes/Form/Resolver/RteNodeResolver.php
+++ packages/in2tiptap/Classes/Form/Resolver/RteNodeResolver.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace In2code\In2TipTap\Form\Resolver;
 
+use In2code\In2TipTap\Exception\MissingEditorConfigurationException;
 use In2code\In2TipTap\Form\Element\TipTapTextElement;
 use Symfony\Component\DependencyInjection\Attribute\Autoconfigure;
 use TYPO3\CMS\Backend\Form\NodeResolverInterface;
@@ -19,7 +20,6 @@ class RteNodeResolver implements NodeResolverInterface
 
     public function setData(array $data): void
     {
-        // TODO: resolve editor configuration
         $this->data = $data;
     }
 
@@ -27,25 +27,54 @@ class RteNodeResolver implements NodeResolverInterface
      * Returns RichTextElement as class name if RTE widget should be rendered.
      *
      * @return string|null New class name or null if this resolver does not change current class name.
+     * @throws MissingEditorConfigurationException
      */
     public function resolve(): ?string
     {
         $parameterArray = $this->data['parameterArray'];
 
-        if ((bool)($parameterArray['fieldConf']['config']['enableRichtext'] ?? false) === false
-            // If RTE config is found (prepared by TcaText data provider)
-            && is_array($parameterArray['fieldConf']['config']['richtextConfiguration'] ?? null) === false
-            // If RTE is not disabled on configuration level
-            && ($parameterArray['fieldConf']['config']['richtextConfiguration']['disabled'] ?? false)
-        ) {
+        if ($this->shouldRenderRichtext($parameterArray) === false) {
             return null;
         }
 
         if ($this->features->isFeatureEnabled('rte.tiptap')) {
+            $this->setProperRichtextConfiguration('tiptap');
             return TipTapTextElement::class;
         }
 
-        // default ckeditor
         return RichTextElement::class;
+
+    }
+
+    /**
+     * Could be extended in the future to split up editor configurations like: editor.tiptap, editor.ckeditor, ...
+     * @throws MissingEditorConfigurationException
+     */
+    private function setProperRichtextConfiguration(string $editor): void
+    {
+        if (!isset($this->data['parameterArray']['fieldConf']['config']['richtextConfiguration']['editor'][$editor]['config'])) {
+            throw new MissingEditorConfigurationException(
+                'Missing editor configuration for editor: ' . $editor,
+                1755159351
+            );
+        }
+
+        $this->data['parameterArray']['fieldConf']['config']['richtextConfiguration']['editor'] =
+            $this->data['parameterArray']['fieldConf']['config']['richtextConfiguration']['editor'][$editor];
+    }
+
+    private function shouldRenderRichtext(array $parameterArray): bool
+    {
+        $config = $parameterArray['fieldConf']['config'] ?? null;
+        if (!is_array($config)) {
+            return false;
+        }
+
+        // If RTE is enabled for field
+        return ((bool)($config['enableRichtext'] ?? false) === true)
+            // If RTE config is found (prepared by TcaText data provider)
+            && is_array($config['richtextConfiguration'] ?? null)
+            // If RTE is not disabled on configuration level
+            && !($config['richtextConfiguration']['disabled'] ?? false);
     }
 }
