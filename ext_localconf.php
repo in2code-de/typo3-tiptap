6732edca735fce3768fa6d8fdea88acd8cc8b787
Felix Ranesberger <felix.ranesberger@in2code.de>
2025-10-02 12:02:00 +0200

    [BUGFIX] Force write in2tiptap configuration to default preset to make sure we don't use the ckeditor preset

--- ext_localconf.php
+++ ext_localconf.php
@@ -9,6 +9,4 @@ $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeResolver'][1480314091] = [
     'class' => \In2code\In2TipTap\Form\Resolver\RteNodeResolver::class,
 ];
 
-if (empty($GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['default'])) {
-    $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['default'] = 'EXT:in2tiptap/Configuration/RTE/Full.yaml';
-}
+$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['default'] = 'EXT:in2tiptap/Configuration/RTE/Full.yaml';
