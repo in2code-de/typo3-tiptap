14ed34a89e5f14bd31fa873b40c80e3d59101b0a
Bastien Lutz <bastien.lutz@in2code.de>
2025-10-02 10:59:56 +0200

    [BUGFIX] Prevent issues when overwriting RTE configuration

--- ext_localconf.php
+++ ext_localconf.php
@@ -9,4 +9,6 @@ $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeResolver'][1480314091] = [
     'class' => \In2code\In2TipTap\Form\Resolver\RteNodeResolver::class,
 ];
 
-$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['default'] = 'EXT:in2tiptap/Configuration/RTE/Full.yaml';
+if (empty($GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['default'])) {
+    $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['default'] = 'EXT:in2tiptap/Configuration/RTE/Full.yaml';
+}
