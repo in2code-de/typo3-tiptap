32a9642532b270c24322b925a68bb2b513561eaf
Felix Ranesberger <felix.ranesberger@in2code.de>
2025-11-24 14:22:32 +0100

    [BUGFIX] Calculate character count based on dom parser, in order to count only actual text content and not html tags

--- frontend/src/plugins/character-count.ts
+++ frontend/src/plugins/character-count.ts
@@ -14,10 +14,23 @@ export default function (unsafeConfig: unknown) {
     }),
   })
 
+  function getAccurateTextLength(htmlString: string) {
+    const parser = new DOMParser()
+    const doc = parser.parseFromString(htmlString, 'text/html')
+
+    // Use innerText instead of textContent
+    // innerText respects CSS rendering and whitespace collapsing
+    // eslint-disable-next-line unicorn/prefer-dom-node-text-content
+    return doc.body.innerText.length
+  }
+
   defineTipTapPlugin({
     extensions: [
       CharacterCount.configure({
         limit: config.limit,
+        textCounter: (text) => {
+          return getAccurateTextLength(text)
+        },
       }),
     ],
   })
